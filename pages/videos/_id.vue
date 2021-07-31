4271c70b4458bef7558719280a3bf128d1d3589d
JoBenEtuk <JoBenEtuk@users.noreply.github.com>
2021-07-31 07:52:26 +0100

    init

---
+++ pages/videos/_id.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>We are in video {{ $route.params.id }}</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ pages/videos/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Videos</h1>
+    <div v-for="video in videos" :key="video.id">
+      <h3>{{ video.name }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      videos: [
+        {
+          id: 16,
+          name: "Introduction to Nuxt JS",
+        },
+        {
+          id: 19,
+          name: "Introduction to Vue JS",
+        },
+        {
+          id: 71,
+          name: "Introduction to JS",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
